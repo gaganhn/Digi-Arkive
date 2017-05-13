e9a67742e5e4cad5c458558cbde617da98933ce7
gagan <gaganhn.incredible@gmail.com>
2017-05-13 14:52:10 -0700

    Home page changes

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,31 @@ $("a.navitems").on('click', function(event) {
         });
     } // End if
 });
+
+
+$("#card").flip({
+    trigger: 'manual'
+});
+
+
+$(".signup_link").click(function() {
+
+    $(".signin_form").css('opacity', '0');
+    $(".signup_form").css('opacity', '100');
+
+
+    $("#card").flip(true);
+
+    return false;
+});
+
+$("#unflip-btn").click(function() {
+
+    $(".signin_form").css('opacity', '100');
+    $(".signup_form").css('opacity', '0');
+
+    $("#card").flip(false);
+
+    return false;
+
+});
